69e2cef4eeca836e60f98fa6a72d34c6e2f9ed6e
marcin10lw <marcin10lw@wp.pl>
2023-04-26 14:42:16 +0200

    configured scss and typescript

---
+++ src/custom.d.ts
@@ -0,0 +1,12 @@
+declare module "*.svg" {
+  import * as React from "react";
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  const src: string;
+  export default src;
+  export default content;
+}
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.module.scss";
