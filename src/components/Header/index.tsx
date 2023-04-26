3f3724d0b3f42445dab31a2388d63b5c582c356c
marcin10lw <marcin10lw@wp.pl>
2023-04-26 21:30:26 +0200

    styled Header mobile view

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,18 +3,26 @@ import NavigationList from "../NavigationList";
 import styles from "./index.module.scss";
 import { ReactComponent as CartIcon } from "./icon-cart.svg";
 import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as Hamburger } from "./icon-hamburger.svg";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__wrapper}>
+        <button
+          className={`${styles.header__button} ${styles["header__button--hamburger"]}`}
+        >
+          <Hamburger />
+        </button>
+        <Link to="home" className={styles.header__logo}>
+          <Logo />
+        </Link>
         <nav className={styles.header__nav}>
-          <Link to="home">
-            <Logo className={styles.header__logo} />
-          </Link>
           <NavigationList />
         </nav>
-        <button className={styles.header__cart}>
+        <button
+          className={`${styles.header__button} ${styles["header__button--cart"]}`}
+        >
           <CartIcon />
         </button>
       </div>
