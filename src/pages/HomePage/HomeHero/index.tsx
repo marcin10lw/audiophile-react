d6a172f8cd6da39485631d547c1211a7585a8ecd
marcin10lw <marcin10lw@wp.pl>
2023-04-28 21:44:13 +0200

    animated hero article with framer motion

--- src/pages/HomePage/HomeHero/index.tsx
+++ src/pages/HomePage/HomeHero/index.tsx
@@ -1,34 +1,54 @@
-import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 import Container from "../../../components/Container";
-import { Product, selectProductsByName } from "../../../productsSlice";
-import { RootState } from "../../../store";
 import styles from "./index.module.scss";
+import { Link } from "react-router-dom";
+
+const articleVariants = {
+  hidden: {
+    opacity: 0,
+    y: -30,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.2,
+      duration: 1,
+    },
+  },
+};
 
 const HomeHero = () => {
   return (
-    <>
-      <section className={styles.heroSection}>
-        <Container>
-          <div className={styles.wrapper}>
-            <div className={styles.heroImage} />
-            <article className={styles.heroArticle}>
-              <div className={styles.heroArticle__info}>NEW PRODUCT</div>
+    <section className={styles.heroSection}>
+      <Container>
+        <div className={styles.wrapper}>
+          <motion.div className={styles.heroImage} />
+          <motion.article
+            variants={articleVariants}
+            initial="hidden"
+            animate="visible"
+            className={styles.heroArticle}
+          >
+            <div className={styles.heroArticle__info}>NEW PRODUCT</div>
 
-              <h2 className={styles.heroArticle__name}>
-                XX99 MARK II HEADPHONES
-              </h2>
-              <p className={styles.heroArticle__description}>
-                Experience natural, lifelike audio and exceptional build quality
-                made for the passionate music enthusiast.
-              </p>
-              <button className={styles.heroArticle__button}>
-                SEE PRODUCT
-              </button>
-            </article>
-          </div>
-        </Container>
-      </section>
-    </>
+            <h2 className={styles.heroArticle__name}>
+              XX99 MARK II HEADPHONES
+            </h2>
+            <p className={styles.heroArticle__description}>
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <Link
+              to="/headphones/xx99-mark-two-headphones"
+              className={styles.heroArticle__link}
+            >
+              SEE PRODUCT
+            </Link>
+          </motion.article>
+        </div>
+      </Container>
+    </section>
   );
 };
 
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,23 +1,14 @@
-import { useSelector } from "react-redux";
-import CathegoryLinks from "../../components/CathegoryLinks";
 import Container from "../../components/Container";
 import HomeHero from "./HomeHero";
-import { selectProductsStatus } from "../../productsSlice";
 
 const HomePage = () => {
-  const status = useSelector(selectProductsStatus);
-
   return (
-    <>
-      {status === "success" && (
-        <main>
-          <HomeHero />
-          <Container wide={true}>
-            <Container></Container>
-          </Container>
-        </main>
-      )}
-    </>
+    <main>
+      <HomeHero />
+      <Container wide={true}>
+        <Container></Container>
+      </Container>
+    </main>
   );
 };
 
