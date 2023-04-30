0b21f39e252a817f305a2b7778d74f7d4867a2fa
marcin10lw <marcin10lw@wp.pl>
2023-05-01 01:42:00 +0200

    styled ZX9 article

--- src/pages/HomePage/ProductsSection/index.tsx
+++ src/pages/HomePage/ProductsSection/index.tsx
@@ -5,20 +5,23 @@ const ProductsSection = () => {
   return (
     <section className={styles.productsSection}>
       <article className={styles.zx9Article}>
-        <img
-          className={styles.zx9Article__image}
-          src={require("./images/desktop/image-speaker-zx9.png")}
-          alt="zx9 speakers"
-        />
-        <div className={styles.infoWrapper}>
-          <h2 className={styles.zx9Article__name}>ZX9 SPEAKER</h2>
-          <p className={styles.zx9Article__description}>
-            Upgrade to premium speakers that are phenomenally built to deliver
-            truly remarkable sound.
-          </p>
-          <Link to="/speakers/zx9-speaker" className={styles.link}>
-            SEE PRODUCT
-          </Link>
+        <div className={styles.zx9Article__flex}>
+          <img
+            className={styles.zx9Article__image}
+            src={require("./images/desktop/image-speaker-zx9.png")}
+            alt="zx9 speakers"
+            draggable={false}
+          />
+          <div className={styles.infoWrapper}>
+            <h2 className={styles.zx9Article__name}>ZX9 SPEAKER</h2>
+            <p className={styles.zx9Article__description}>
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </p>
+            <Link to="/speakers/zx9-speaker" className={styles.link}>
+              SEE PRODUCT
+            </Link>
+          </div>
         </div>
       </article>
     </section>
