f695d37bf8d76153518031ba43c72ce9bc136dc9
marcin10lw <marcin10lw@wp.pl>
2023-05-02 23:27:03 +0200

    created GearSection and styled it

---
+++ src/pages/HomePage/GearSection/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./index.module.scss";
+
+const GearSection = () => {
+  return (
+    <section className={styles.section}>
+      <article className={styles.article}>
+        <h2 className={styles.article__title}>
+          BRINGING YOU THE <strong>BEST</strong> AUDIO GEAR
+        </h2>
+        <p className={styles.article__description}>
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </article>
+      <img className={styles.section__image} alt="best gear" />
+    </section>
+  );
+};
+
+export default GearSection;
