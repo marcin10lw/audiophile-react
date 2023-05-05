6259c09dc701d1a55843673e93626b6e0d34affe
marcin10lw <marcin10lw@wp.pl>
2023-05-05 23:52:26 +0200

    simplified NavLinks component

--- src/components/NavigationList/index.tsx
+++ src/components/NavigationList/index.tsx
@@ -1,21 +1,28 @@
 import { NavLink } from "react-router-dom";
 import styles from "./index.module.scss";
 
+const ListElement = ({ path, title }: { path: string; title: string }) => (
+  <li>
+    <NavLink
+      to={path}
+      className={({ isActive }) =>
+        isActive
+          ? `${styles.navlink} ${styles["navlink--active"]}`
+          : styles.navlink
+      }
+    >
+      {title.toLocaleUpperCase()}
+    </NavLink>
+  </li>
+);
+
 const NavigationList = () => {
   return (
     <ul className={styles.list}>
-      <li>
-        <NavLink to="/home">HOME</NavLink>
-      </li>
-      <li>
-        <NavLink to="/category/headphones">HEADPHONES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/category/speakers">SPEAKERS</NavLink>
-      </li>
-      <li>
-        <NavLink to="/category/earphones">EARPHONES</NavLink>
-      </li>
+      <ListElement path="/home" title="home" />
+      <ListElement path="/category/headphones" title="headphones" />
+      <ListElement path="/category/speakers" title="speakers" />
+      <ListElement path="/category/earphones" title="earphones" />
     </ul>
   );
 };
