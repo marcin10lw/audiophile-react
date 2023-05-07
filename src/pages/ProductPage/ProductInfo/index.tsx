b85e7345e8a58e1c8210594aa2553524812a6729
marcin10lw <marcin10lw@wp.pl>
2023-05-07 21:30:15 +0200

    created and styled ProductInfo

---
+++ src/pages/ProductPage/ProductInfo/index.tsx
@@ -0,0 +1,33 @@
+import styles from "./index.module.scss";
+
+type ProductInfoProps = {
+  features: string;
+  included: {
+    quantity: number;
+    item: string;
+  }[];
+};
+
+const ProductInfo = ({ features, included }: ProductInfoProps) => {
+  return (
+    <section className={styles.productInfo}>
+      <article className={styles.features}>
+        <h2 className={styles.heading}>FEATURES</h2>
+        <p className={styles.productInfo__text}>{features}</p>
+      </article>
+      <article className={styles.box}>
+        <h2 className={styles.heading}>IN THE BOX</h2>
+        <ul className={styles.box__list}>
+          {included.map(({ item, quantity }) => (
+            <li key={item} className={styles.box__item}>
+              <span className={styles.quantity}>{quantity}x</span>
+              <span className={styles.item}>{item}</span>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </section>
+  );
+};
+
+export default ProductInfo;
