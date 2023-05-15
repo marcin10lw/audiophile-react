416a8c74315d2e95a74f75e025b4471738745a67
marcin10lw <marcin10lw@wp.pl>
2023-05-15 23:56:49 +0200

    styled radio inputs

--- src/pages/CheckoutPage/CheckoutForm/index.tsx
+++ src/pages/CheckoutPage/CheckoutForm/index.tsx
@@ -92,26 +92,28 @@ const CheckoutForm = () => {
         <legend className={styles.legend}>PAYMENT DETAILS</legend>
         <div>
           <div>Payment Method</div>
-          <div>
-            <label className={styles.radioLabel}>
-              <input
-                name="payment"
-                defaultChecked={true}
-                type="radio"
-                value={"eMoney"}
-                className={styles.radioInput}
-              />
+          <div className={styles.radioWrapper}>
+            <input
+              id="radio1"
+              name="payment"
+              defaultChecked={true}
+              type="radio"
+              value={"eMoney"}
+              className={styles.radioInput}
+            />
+            <label className={styles.radioLabel} htmlFor="radio1">
               e-Money
             </label>
           </div>
-          <div>
-            <label className={styles.radioLabel}>
-              <input
-                name="payment"
-                type="radio"
-                value={"cashOnDelivery"}
-                className={styles.radioInput}
-              />
+          <div className={styles.radioWrapper}>
+            <input
+              id="radio2"
+              name="payment"
+              type="radio"
+              value={"cashOnDelivery"}
+              className={styles.radioInput}
+            />
+            <label htmlFor="radio2" className={styles.radioLabel}>
               Cash on Delivery
             </label>
           </div>
