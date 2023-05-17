fa43624543b0220e70abb638f926984b05aa3b41
marcin10lw <marcin10lw@wp.pl>
2023-05-17 15:29:58 +0200

    gave calculated information in Summary

--- src/pages/CheckoutPage/Summary/index.tsx
+++ src/pages/CheckoutPage/Summary/index.tsx
@@ -1,26 +1,35 @@
+import { useSelector } from "react-redux";
 import CartList from "../../../common/CartList";
 import styles from "./index.module.scss";
+import { selectTotalPrice } from "../../../store/cartSlice";
+import { formatPrice } from "../../../utils/formatPrice";
 
 const Summary = () => {
+  const totalPrice = useSelector(selectTotalPrice);
+  const shippingPrice = 50;
+  const vatRate = 0.2;
+  const vat = totalPrice * vatRate;
+  const grandTotal = totalPrice + shippingPrice;
+
   return (
     <section className={styles.summary}>
       <h2 className={styles.summary__heading}>SUMMARY</h2>
       <CartList location="checkout" />
       <div className={styles.info}>
         <p className={styles.info__detail}>
-          <span>TOTAL</span>$ 5,396
+          <span>TOTAL</span>$ {formatPrice(totalPrice)}
         </p>
         <p className={styles.info__detail}>
-          <span>SHIPPING</span>$ 5,396
+          <span>SHIPPING</span>$ {shippingPrice}
         </p>
         <p className={styles.info__detail}>
-          <span>VAT (INCLUDED)</span>$ 5,396
+          <span>VAT (INCLUDED)</span>$ {formatPrice(vat)}
         </p>
 
         <p
           className={`${styles.info__detail} ${styles["info__detail--grandTotal"]}`}
         >
-          <span>GRAND TOTAL</span>$ 5,396
+          <span>GRAND TOTAL</span>$ {formatPrice(grandTotal)}
         </p>
       </div>
       <button className={styles.summary__button} type="submit">
